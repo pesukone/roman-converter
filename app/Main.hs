ecdb091661a742b5bd6e02925a61c8de730b0cb4
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2018-11-25 14:33:12 +0200

    refactored and allowed lowercase symbols

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,14 @@
 module Main where
 
-import Converter
-import Validation
+import Converter (toIntArray, toNum)
+import Validation (validate)
+import Data.Char
 
 main :: IO ()
 main = do
     putStrLn "Enter a Roman number:"
     number <- getLine
-    let numberArray = validate $ sequence $ toIntArray number
+    let numberArray = validate $ sequence $ toIntArray (map toUpper number)
     putStrLn $ case numberArray of
                 Nothing -> "The number is invalid"
                 Just x -> show $ toNum x
